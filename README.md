a35cf996cefc0fb662deb53054864aa0461198d4
Alan Knight <alanknight@google.com>
2014-08-06 15:54:25 -0700

    First draft

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple example of a minimal serialization in Dart. It goes with
+minimal_serialization_example.
---
+++ lib/test_models.dart
@@ -0,0 +1,13 @@
+library test_models;
+
+class Thing {
+  int howMany;
+  String name;
+  List<String> things;
+}
+
+class OtherThing {
+  int a, b;
+  var c;
+  Map map;
+}
